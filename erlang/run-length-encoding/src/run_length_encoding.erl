65001dd29a79cf937670da664799fd2838af754e
Sergey Karpuk <sergeykarpuk@yandex.by>
2025-11-12 17:14:11 +0300

    feature(erlang): add run_length_encoding solution

--- erlang/run-length-encoding/src/run_length_encoding.erl
+++ erlang/run-length-encoding/src/run_length_encoding.erl
@@ -3,6 +3,28 @@
 -export([decode/1, encode/1]).
 
 
-decode(_String) -> undefined.
+decode("") -> "";
+decode([Head | Tail] = String) ->
+	case string:to_integer(String) of
+		{error, no_integer} -> [Head | decode(Tail)];
+		{Length, [Char | T]} -> lists:duplicate(Length, Char) ++ decode(T)
+	end.
 
-encode(_String) -> undefined.
+encode("") -> "";
+encode(Str) ->
+  {match, Groups} = re:run(Str, "(.)\\1*", [global]),
+
+  lists:foldl(fun ([{I, Len}, _], Acc) ->
+							Length = if
+												 Len == 1 -> [];
+												 true -> integer_to_list(Len)
+											 end,
+							Acc ++ Length ++ [lists:nth(I + 1, Str)]
+              end, [], Groups).
+
+
+% re:compile("^[a-z]$").
+% re:inspect(MP, namelist).
+
+% re:replace("asdf", "a", "1", [{return, list}]).
+% re:replace("asdf", "a", "1", [{return, binary}]).
