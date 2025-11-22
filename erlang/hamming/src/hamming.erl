337321157d23858e6f7389f899cdf0719f448290
Sergey Karpuk <sergeykarpuk@yandex.by>
2025-11-22 14:02:05 +0300

    feature(erlang): add hamming solution

--- erlang/hamming/src/hamming.erl
+++ erlang/hamming/src/hamming.erl
@@ -3,4 +3,11 @@
 -export([distance/2]).
 
 
-distance(_Strand1, _Strand2) -> undefined.
+distance(Strand1, Strand2) ->
+  try lists:zip(Strand1, Strand2) of
+    Pairs when is_list(Pairs) ->
+      Errors = lists:filter(fun({Nuc1, Nuc2}) -> Nuc1 =/= Nuc2 end, Pairs),
+      length(Errors)
+    catch
+      error:_ -> {error, badarg}
+  end.
