8f02061a3c5907d8e908f590e8e367984914ea72
Sergey Karpuk <sergeykarpuk@yandex.by>
2025-11-22 17:53:09 +0300

    refactor(hamming): use foldl

--- erlang/hamming/src/hamming.erl
+++ erlang/hamming/src/hamming.erl
@@ -3,11 +3,20 @@
 -export([distance/2]).
 
 
+% distance(Strand1, Strand2) ->
+%   try lists:zip(Strand1, Strand2) of
+%     Pairs when is_list(Pairs) ->
+%       Errors = lists:filter(fun({Nuc1, Nuc2}) -> Nuc1 =/= Nuc2 end, Pairs),
+%       length(Errors)
+%     catch
+%       error:_ -> {error, badarg}
+%   end.
+
 distance(Strand1, Strand2) ->
-  try lists:zip(Strand1, Strand2) of
-    Pairs when is_list(Pairs) ->
-      Errors = lists:filter(fun({Nuc1, Nuc2}) -> Nuc1 =/= Nuc2 end, Pairs),
-      length(Errors)
+  try
+    lists:foldl(fun({X, X}, Errors_count) -> Errors_count;
+                   (_, Errors_count) -> Errors_count + 1 end,
+                0, lists:zip(Strand1, Strand2))
     catch
       error:_ -> {error, badarg}
   end.
